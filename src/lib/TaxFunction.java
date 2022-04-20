14d71433600c21166d4b1cfdb1f1608be0cfad19
Fathudinashari <fath.ashari@gmail.com>
2022-04-20 21:39:51 +0700

    Menghapus comment pada seluruh class
    Menghapus comment pada seluruh class

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -65,7 +65,6 @@ public class TaxFunction {
 	
 	public int getAnnualIncomeTax() {
 		
-		//Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
 		LocalDate date = LocalDate.now();
 		
 		if (date.getYear() == yearJoined) {
